5446a5902cee7907cfbc64b61f5763a3b519f5bb
Fabio Baccarin <fabiogbaccarin@gmail.com>
2022-03-20 07:09:42 -0300

    split data

--- scripts/split.py
+++ scripts/split.py
@@ -1,5 +1,17 @@
 """
 Splits data into train and test sets
+
+Inputs:
+    - data/X.pkl
+    - data/X_selected.pkl
+    - data/y.pkl
+Outputs:
+    - data/X_train.pkl
+    - data/X_s_train.pkl
+    - data/X_test.pkl
+    - data/X_s_test.pkl
+    - data/y_train.pkl
+    - data/y_test.pkl
 """
 
 # Proportions:
@@ -13,14 +25,16 @@ def main():
     from sklearn import model_selection
 
     X = pd.read_pickle(paths.DATA.joinpath("X.pkl"))
+    X_selected = pd.read_pickle(paths.DATA.joinpath("X_selected.pkl"))
     y = pd.read_pickle(paths.DATA.joinpath("y.pkl"))
 
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        X, y,
-        test_size=0.3,
-        random_state=0,
-        stratify=y,
-    )
+    X_train, X_test, X_s_train, X_s_test, y_train, y_test = \
+        model_selection.train_test_split(
+            X, X_selected, y,
+            test_size=0.3,
+            random_state=0,
+            stratify=y,
+        )
 
     # Sanity checks
     REL_TOL = 0.01
@@ -49,9 +63,14 @@ def main():
         rel_tol=REL_TOL,
         abs_tol=ABS_TOL
     )
+    assert X_s_train.index.equals(X_train.index)
+    assert X_s_test.index.equals(X_test.index)
+
     # Saving data
     X_train.to_pickle(paths.DATA.joinpath("X_train.pkl"))
     X_test.to_pickle(paths.DATA.joinpath("X_test.pkl"))
+    X_s_train.to_pickle(paths.DATA.joinpath("X_s_train.pkl"))
+    X_s_test.to_pickle(paths.DATA.joinpath("X_s_test.pkl"))
     y_train.to_pickle(paths.DATA.joinpath("y_train.pkl"))
     y_test.to_pickle(paths.DATA.joinpath("y_test.pkl"))
 
