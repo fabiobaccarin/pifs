75f7f4003a18208076493622acd3962c75a7a494
Fabio Baccarin <fabiogbaccarin@gmail.com>
2022-03-20 07:14:05 -0300

    update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -7,8 +7,10 @@ if __name__ == "__main__":
     import data_profile
     import preprocessing
     import split
+    import train
 
     colnames.main()
     data_profile.main()
     preprocessing.main()
     split.main()
+    train.main()
